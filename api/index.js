059a105208db321c882df1307a24d7ccc80402d9
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-09-02 21:05:36 +0100

    Add route to get back all movies

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const movies = require('./movies');
 const mongoose = require('mongoose');
-require('dotenv/').config();
+require('dotenv').config();
 
 const app = express();
 
--- api/movies.js
+++ api/movies.js
@@ -1,17 +1,31 @@
-Movie = require('./models/Movie');
+const Movie = require('./models/Movie');
+
 const movies = require('express').Router();
 
 // All movies
-movies.get('/', (req, res) => res.send('All movies'));
+movies.get('/', async (req, res) => {
+	try {
+		const movies = await Movie.find();
+		res.json(movies);
+	} catch (err) {
+		res.json({ message: err });
+	}
+});
 
 // Add a movie
-movies.post('/', (req, res) => {
+movies.post('/', async (req, res) => {
 	const movie = new Movie({
 		title: req.body.title,
 		rated: req.body.rated,
 		year: req.body.year,
 	});
-	movie.save().then(() => res.send('movie saved'));
+
+	try {
+		const savedMovie = await movie.save();
+		res.send(savedMovie);
+	} catch (err) {
+		res.json({ message: err });
+	}
 });
 
 module.exports = movies;
