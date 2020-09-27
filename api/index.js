45f3ed3459ac6e3605a621ea213981d9808b848d
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-09-27 13:39:40 +0100

    Add a default route for wild requests

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const PORT = 3000;
+
 const app = express();
 
 // Middlewares
@@ -13,7 +15,14 @@ app.use('/api/movies', movies);
 
 // Home route
 app.get('/api', (req, res) => {
-	res.send('hitting the home page');
+	res.send('<h1 style="font-family:sans-serif">hitting the home page</h1>');
+});
+
+// Catch wild requests
+app.use((req, res) => {
+	res
+		.status(404)
+		.send('<h1 style="font-family:sans-serif">Error 404: Not found</h1>');
 });
 
 // Connect to DB
@@ -27,4 +36,4 @@ db.once('open', () => console.log('Connected to the database'));
 db.on('error', () => console.log('Failed to connect to the database'));
 
 // Listening to the server
-app.listen(3000, () => console.log('Server Running'));
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
